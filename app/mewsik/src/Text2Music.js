55cce7080723678a7b2305d22fed61d08ae79663
Katherine Liu <shigongliu@dhcp-10-31-123-39.dyn.mit.edu>
2022-01-19 03:18:45 -0500

    add styles to text2music page

--- app/mewsik/src/Text2Music.js
+++ app/mewsik/src/Text2Music.js
@@ -2,7 +2,13 @@ import './Text2Music.css';
 
 function Text2Music() {
     return(
-        <div> hi :) </div>
+        <div className="flex-container"> 
+            <h2> hi :) </h2>
+            <div className="text-container">
+                <textarea rows="20" cols="80" className="text"></textarea>
+            </div>
+            <button className="next-button">submit!</button>
+        </div>
     );
 }
 
