6dda2e74267a290d27603a9eb5f2ea7a498e7a68
Katherine Liu <shigongliu@dhcp-10-31-123-39.dyn.mit.edu>
2022-01-23 02:56:38 -0500

    add music library

--- app/mewsik/src/Music.js
+++ app/mewsik/src/Music.js
@@ -1,6 +1,14 @@
 import './Music.css';
 
+import * as Tone from 'tone'
+
 function Music(props) {
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease("C4", "8n");
+
     return(
         <div>
             <h2> hi :D </h2>
